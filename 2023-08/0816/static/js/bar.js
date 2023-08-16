6346d548599407e34b6c63e5bc103e0cef2feca1
yerim-je <a01023041704@gmail.com>
2023-08-16 16:26:19 +0900

    누적데이터

--- 2023-08/0816/static/js/bar.js
+++ 2023-08/0816/static/js/bar.js
@@ -1,21 +1,34 @@
 const tall=[161,168,174,159];
 const name=["송은선","임민지","이상준","김선향"];
+const weight=[43,59,72,46];
 
-const avg = tall_avg();
+let avg = tall_avg();
 $("#reg_bt").click(function(){
-    if( $("#tall").val()=='' || $("#name".val()=='')){
-        alert("키 또는 이름을 입력하시오.");
+    if( $("#tall").val()=='' || $("#name").val()=='' ){
+        alert("키 또는 이름을 입력하세요");
         $("#tall").val()=='' ? $("#tall").focus():$("#name").focus();
+        return;
+    }
+    if( $("#weight").val()==''){
+        alert("몸무게를 입력하시오.");
+        $("#weight").focus();
         return;
     }
     tall.push( Number($("#tall").val()) );
     name.push( $("#name").val() );
+    weight.push(  $("#weight").val() );
+
+    avg=tall_avg();
+    chart.destroy();
+    draw();
+    $("#tall").val("");
+    $("#name").val("").focus();
+    
 });
 
 const ctx=$("#bar_chart")[0].getContext("2d");
 let chart='';
 draw();
-
 
 function draw(){
     chart = new Chart(ctx,{
@@ -23,40 +36,37 @@ function draw(){
         type:'bar',
         data:{
             labels:name,
-            datasets:[{
-                label:"201호 키 조사",
-                data:tall,
-                borderWidth:1,
-                backgroundColor:[
-                    tall[0]>=avg ? "#A15D74":"#75AE97",
-                    tall[1]>=avg ? "#A15D74":"#75AE97",
-                    tall[2]>=avg ? "#A15D74":"#75AE97",
-                    tall[3]>=avg ? "#A15D74":"#75AE97"
-                ],
-                barPercentage:0.5,
-                barThickness:50,
-                borderColor:"black",
-                borderSkipped:"start",
-                borderRadius:5,
-                categoryPercentage:0.1,
-                hoverBackgroundColor:"yellow",
-                indexAxis:"x",
-                base:avg
-            }]
+            datasets:[
+                {
+                    label:"201호 키 조사",
+                    data:tall,
+                    borderWidth:1,
+                    backgroundColor: avg_color(),
+                    // tall.map(function(t) {return t >= avg ? '#FF8F6A' : '#79B1BF';}),
+                    barPercentage:0.5,
+                    indexAxis:"y"
+                },
+                {
+                    label:"201호 몸무게 조사",
+                    data:weight,
+                    borderWidth:1,
+                    backgroundColor:"yellow",
+                    // tall.map(function(t) {return t >= avg ? '#FF8F6A' : '#79B1BF';}),
+                    barPercentage:0.5,
+                    indexAxis:"y"
+                }
+            ]
         },
         options:{
-            scales:{
-                y:{
-                    min:150,max:200,
-                    ticks:{
-                        stepSize:10
-                    }
-                }
-            }
+            
         }
     });
 }
-
+function avg_color(){
+    var a=[];
+    $.each(tall,function(i,t){a.push(t >= avg ? '#79B1BF' : '#FF8F6A' )});
+    return a;
+}
 function tall_avg(){
     var sum=0;
     $.each(tall, function(i,t){

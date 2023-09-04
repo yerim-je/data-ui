a0f7e96e9b2e990d74c97939c4f3211405e75f3d
yerim-je <a01023041704@gmail.com>
2023-09-04 17:25:32 +0900

    vue.js

---
+++ nodeJS/vuejs1/src/router.js
@@ -0,0 +1,31 @@
+
+
+import { createWebHistory, createRouter } from 'vue-router';
+
+const routes =[
+    {
+        path:'/',
+        name:"MyHome",
+        component:() => import('@/components/MyHome.vue')
+    },
+    {
+        path:"/login",
+        name:"SdmLogin",
+        component:() => import('@/components/SdmLogin.vue')
+    },
+    {
+        path:"/sign",
+        name:"SdmSign",
+        component: () => import('@/components/SdmSign.vue')
+    },
+    {
+        path:"/qs",
+        name:"SdmQs",
+        component:() => import('@/components/SdmQs.vue')
+    }
+];
+
+const router = createRouter({
+    history : createWebHistory(), routes,
+});
+export default router;

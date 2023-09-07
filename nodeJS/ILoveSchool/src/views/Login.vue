7065a5c243c225b45267a1de262a0a447157f119
yerim-je <a01023041704@gmail.com>
2023-09-07 15:28:22 +0900

    페이스북 로그인

--- nodeJS/ILoveSchool/src/views/Login.vue
+++ nodeJS/ILoveSchool/src/views/Login.vue
@@ -4,23 +4,46 @@
         <input type="text" v-model="email" placeholder="Email">
         <input type="password" v-model="password" placeholder="password">
         <button v-on:click="login">로그인</button>
+        <p>또는 페이스북 로그인<br>
+            <button v-on:click="facebookLogin" class="facebook">
+                <img src="@/assets/logo.png" alt="">
+            </button>
+        </p>
         <p>만약 계정이 없다면 <RouterLink to="/signup">회원가입</RouterLink>을 먼저 해주세요</p>
     </div>
 </template>
 <script>
 import { RouterLink } from 'vue-router';
 import firebase from 'firebase'
+
+var provider = new firebase.auth.FacebookAuthProvider()
+provider.addScope('public_profile')
+provider.setCustomParameters({
+    'display':'popup'
+})
+
+
  export default{
     name: 'login',
     data() {
         return {email:'',password:''};
     },
     methods: {
+        facebookLogin(){
+            firebase.auth().signInWithPopup(provider).then(
+                (result) => {
+                    var token = result.credential.accessToken
+                    var user = result.user
+                    alert('로그인 성공')
+                }
+            ).catch( (err) => {alert('에러 : '+err.message)})
+        },
         login(){
             firebase.auth().signInWithEmailAndPassword(this.email, this.password)
             .then(
                 function(user){
                     alert('로그인성공')
+                    this.$router.replace('Msg')
                 },
                 function(err){
                     alert('에러 : '+err.message)
@@ -43,4 +66,12 @@ import firebase from 'firebase'
         padding:5px 15px;
     }
     .login button{height:30px;}
+    .facebook{
+        width:120px;
+        border:0;
+        background:0;
+    }
+    .facebook img{
+        width: 100%;
+    }
 </style>

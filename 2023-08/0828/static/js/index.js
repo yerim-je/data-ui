008961c685d19d28d10d9d670d3969b16a8e1f89
yerim-je <a01023041704@gmail.com>
2023-10-04 16:48:48 +0900

    project2

--- 2023-08/0828/static/js/index.js
+++
@@ -1,9 +0,0 @@
-// import { sum as mySum } from './myModule.js' as를 통해 다은 이름으로 변경ㄹ가능
-import {default as sum,makeTable} from './myModule.js'
-
-console.log(sum(10,20));
-// console.log(mySum(10,20)); -> default 
-window.onload=function(){
-    makeTable("box",4,5);
-    makeTable("box2",3,7);
-}
--- 2023-08/0828/static/js/mdtest.js
+++
@@ -1,9 +0,0 @@
-function output(){
-    console.log("출력테스트");
-}
-
-function output2(){
-    console.log(num);
-}
-
-output2();
